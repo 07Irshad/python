21d397e5ada0035fcfe83f4c106bd9bdae0b16c5
07Irshad <80592277+07Irshad@users.noreply.github.com>
2021-05-29 20:29:18 +0530

    positivenumberss.py

---
+++ positivenumbers.py
@@ -0,0 +1,9 @@
+list1 = [12, -7, 5, 64,-14]
+list2 = [12, 14,-95,3]
+for number1 in list1:	
+	if number1 >= 0:
+	print(number1, end = " ")
+  
+for number2 in list2:	
+	if number2 >= 0:
+	print(number2, end = " ")
